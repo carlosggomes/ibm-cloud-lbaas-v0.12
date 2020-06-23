c870b4e1ea6f68a71d2cf5e62cba2ab1d10a11e6
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-06-23 18:53:51 -0300

    Teste severino
    https://ibm-cloud.github.io/tf-ibm-docs/v1.2.4/d/lbaas.html

--- main.tf
+++ main.tf
@@ -1,33 +1,22 @@
-# Configure the IBM Cloud Provider
-
 resource "ibm_lbaas" "lbaas" {
-  name        = var.name
-  description = "lbaas example"
-  type        = "public"
-  subnets     = [var.subnet_id]
+  name        = "test"
+  description = "updated desc-used for terraform uat"
+  subnets     = [2367048]
+  datacenter  = "dal13"
 
   protocols {
     frontend_protocol     = "HTTP"
     frontend_port         = 80
     backend_protocol      = "HTTP"
     backend_port          = 80
-    load_balancing_method = var.lb_method
-#    tls_certificate_id    = ibm_compute_ssl_certificate.lbaas-ssl-certificate2.id
+    load_balancing_method = "round_robin"
+  }
+
+  server_instances {
+    private_ip_address = "75.126.255.2"
+  }
 }
 
-resource "ibm_lbaas_server_instance_attachment" "lbaas_member" {
-  count              = 1
-  private_ip_address = "75.126.255.2"
-  weight             = 40
-  lbaas_id           = "${ibm_lbaas.lbaas.id}"
-  depends_on         = ["ibm_lbaas.lbaas.id"]
+data "ibm_lbaas" "tfacc_lbaas" {
+  name = "test"
 }
-
-resource "ibm_lbaas_health_monitor" "lbaas_hm" {
-  protocol   = ibm_lbaas.lbaas.health_monitors[0].protocol
-  port       = ibm_lbaas.lbaas.health_monitors[0].port
-  timeout    = 3
-  lbaas_id   = ibm_lbaas.lbaas.id
-  monitor_id = ibm_lbaas.lbaas.health_monitors[0].monitor_id
-}
-
