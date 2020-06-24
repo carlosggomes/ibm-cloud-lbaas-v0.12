0e15e6c359644479fd9f859f69d26ba4f6427dea
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-06-24 01:27:56 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 variable "sl_username" {}
+variable "sl_api_key"{}
 variable "ibmcloud_api_key" {}
 variable "iaas_classic_username" {}
 variable "iaas_classic_api_key" {}
@@ -7,6 +8,7 @@ provider "ibm" {
   generation            = 1
   region                = "us-south"
   sl_username           = "var.sl_username"
+  sl_api_key            = "var.sl_api_key"
   ibmcloud_api_key      = "var.ibmcloud_api_key"
   iaas_classic_username = "var.iaas_classic_username"
   iaas_classic_api_key  = "var.iaas_classic_api_key"
