f0e23fd60db3cd1a1a62c9cff975605b0ca50906
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-06-28 16:06:33 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,18 @@ resource "ibm_lbaas" "lbaas" {
   subnets     = [1850273]
 
   protocols {
-    frontend_protocol     = "HTTP"
-    frontend_port         = 80
+    frontend_protocol     = "HTTPS"
+    frontend_port         = 443
     backend_protocol      = "HTTP"
     backend_port          = 80
     load_balancing_method = "round_robin"
+    tls_certificate_id    = 270088
   }
 }
 
 resource "ibm_lbaas_server_instance_attachment" "lbaas_member" {
-  count              = 2
-  private_ip_address = "10.151.35.202,10.151.35.203"
+  count              = 1
+  private_ip_address = "10.151.35.202"
   weight             = 40
   lbaas_id           = ibm_lbaas.lbaas.id
 }
