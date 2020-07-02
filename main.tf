577e5675c28b124513778cf593f917f16e2ab976
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-07-02 15:56:32 -0300

    count = 2

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "ibm_lbaas" "lbaas" {
 }
 
 resource "ibm_lbaas_server_instance_attachment" "lbaas_member" {
-#  count              = 1
+  count              = 2
   private_ip_address = ["10.151.35.202" , "10.151.35.203"]
   weight             = 40
   lbaas_id           = ibm_lbaas.lbaas.id  
