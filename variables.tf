f3c6b7cb5d9fe5a6171021841822ca967e294084
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-07-05 19:50:47 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+
+
+variable "name" {
+  default = "terraformLbaasExample"
+}
+
+variable "notes" {
+  default = "for lbaas test"
+}
+
+variable "osref" {
+  default = "UBUNTU_16_64"
+}
+
+variable "domain" {
+  default = "ibm.com"
+}
+
+variable "lb_method" {
+  default = "round_robin"
+}
+
+variable "subnet_id" {
+  default = "1851345"
+}
+
+variable "datacenter" {
+  default = "dal10"
+}
+
+variable "vm-post-install-script-uri" {
+  default = "https://raw.githubusercontent.com/hkantare/test/master/nginx.sh"
+}
+
+variable "hostname" {
+  default = "lbaas-example"
+}
+
+© 2020 GitHub, Inc.
