b1c74d19629ca2540c8634912640c7df0a0aa678
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-07-05 21:30:54 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 
-
+#Load Balance name. Will be part of the Load Balance FQDN - DNS entry
 variable "lbaas-name" {
   default = "appname"
 }
@@ -8,30 +8,31 @@ variable "lbaas-notes" {
   default = "Load Balance Description - Private-Private"
 }
 
+#Types are: "round_robin", "weighted_round_robin", "least_connection"
 variable "lb_method" {
   default = "round_robin"
-}
-
-variable "subnet_id" {
-  default = "1851345"
 }
 
 variable "datacenter" {
   default = "sao01"
 }
 
+# To get the TLS / CERTIFICATE ID use the IBM Cloud CLI command >> ibmcloud sl security cert-list
 variable "tls-id" {
   default = "270088"
 }
 
+# Types are: PUBLIC or PRIVATE
 variable "lbaas-type" {
   default = "PRIVATE"
 }
 
+# To get the SUBNET ID use the IBM Cloud CLI command >> ibmcloud sl subnet list
 variable "lbaas-subnet" {
   default = "1850273"
 }
 
+# Numbers of Load Balance Members / IPs
 variable "lbaas-member-count" {
   type = string
   default = "2"
